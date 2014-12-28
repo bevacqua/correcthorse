ea0d84aab4fc56ada2bca9c3989db584febb564b
Jérémy Giuseppi <yereby@gmail.com>
2014-12-28 12:25:50 +0100

    refactor: move random function in correcthorse function scope
    We don't need all in global scope

--- correcthorse.js
+++ correcthorse.js
@@ -1,16 +1,15 @@
 'use strict';
 
-var wordlist = require('./wordlist.json');
-
-function random () {
-  return wordlist[Math.floor(Math.random() * wordlist.length)];
-}
-
 function correcthorse (options) {
   var o = options || {};
   var len = o.length || 10;
   var count = o.words || 4;
   var result = '';
+
+  function random () {
+    var wordlist = require('./wordlist.json');
+    return wordlist[Math.floor(Math.random() * wordlist.length)];
+  }
 
   while (count --> 0 || result.length < len) {
     result += random() + '-';
